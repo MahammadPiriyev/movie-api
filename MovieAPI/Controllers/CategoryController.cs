fd45df5078dd4d40de5a4390e9f490ccd2487e4c
MahammadPiriyev <146362008+MahammadPiriyev@users.noreply.github.com>
2026-03-05 15:55:24 +0400

    test

--- MovieAPI/Controllers/CategoryController.cs
+++ MovieAPI/Controllers/CategoryController.cs
@@ -24,7 +24,7 @@ namespace MovieAPI.Controllers
 			return Ok(movieFromDb);
 		}
 
-		[HttpGet("asdfghjk")]
+		[HttpGet]
 		public async Task<IActionResult> GetAll()
 		{
 			var categoryList = await _unitOfWork.Categories.GetAllAsync();
